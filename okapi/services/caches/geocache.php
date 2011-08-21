c8304780f5c9429d6445c7fdb530b996f2093b19
rygielski <rygielski@gmail.com>
2011-08-21 10:18:04 +0000

    Renamed "OX codes" with "Cache waypoints". Changed method signatures and internal variable names and comments.

--- okapi/services/caches/geocache.php
+++ okapi/services/caches/geocache.php
@@ -23,20 +23,20 @@ class WebService
 
 	public static function call(OkapiRequest $request)
 	{
-		$oxcode = $request->get_parameter('oxcode');
-		if (!$oxcode) throw new ParamMissing('oxcode');
+		$cache_wpt = $request->get_parameter('cache_wpt');
+		if (!$cache_wpt) throw new ParamMissing('cache_wpt');
 		$fields = $request->get_parameter('fields');
-		if (!$fields) $fields = "oxcode|name|location|type|status";
+		if (!$fields) $fields = "wpt|name|location|type|status";
 		
 		# There's no need to validate the fields parameter as the 'geocaches'
 		# method does this (it will raise a proper exception on invalid values).
 		
 		$results = OkapiServiceRunner::call('services/caches/geocaches', new OkapiInternalRequest(
-			$request->consumer, $request->token, array('oxcodes' => $oxcode,
+			$request->consumer, $request->token, array('cache_wpts' => $cache_wpt,
 			'fields' => $fields)));
-		$result = $results[$oxcode];
+		$result = $results[$cache_wpt];
 		if ($result == null)
-			throw new InvalidParam('oxcode', "There is no geocache by this OX code.");
+			throw new InvalidParam('cache_wpt', "There is no geocache by this waypoint code.");
 		return Okapi::formatted_response($request, $result);
 	}
 }
