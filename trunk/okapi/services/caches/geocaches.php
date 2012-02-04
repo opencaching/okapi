6e126a222e42a38eb670f46555e5fe81be27accc
rygielski@gmail.com <rygielski@gmail.com@9d75f559-08eb-57be-2bd7-6ad0879b0bc2>
2012-02-04 17:14:33 +0000

    Update issue 48

--- trunk/okapi/services/caches/geocaches.php
+++ trunk/okapi/services/caches/geocaches.php
@@ -462,10 +462,11 @@ class WebService
 			while ($row = mysql_fetch_assoc($rs))
 			{
 				$results[$cacheid2wptcode[$row['cache_id']]]['alt_wpts'][] = array(
-					'name' => ($row['stage'] ? _("Stage")." ".$row['stage'].": " : "").$row['desc'],
+					'name' => $cacheid2wptcode[$row['cache_id']]."-".($row['stage'] ? "-".$row['stage'] : "-wpt"),
 					'location' => round($row['latitude'], 6)."|".round($row['longitude'], 6),
 					'sym' => (($row['type'] == 3) ? "Flag, Red" : ($row['type'] == 4) ? "Circle with X" :
 						($row['type'] == 5) ? "Parking Area" : "Flag, Green"),
+					'description' => ($row['stage'] ? _("Stage")." ".$row['stage'].": " : "").$row['desc'],
 				);
 			}
 		}
