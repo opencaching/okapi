9057593ea97a1df538ba97e39109f25ad38dc2e3
kkarczmarczyk <kkarczmarczyk@gmail.com>
2012-02-10 18:28:04 +0000

    [spaces] => [tabs] ;)

--- okapi/services/caches/geocaches.php
+++ okapi/services/caches/geocaches.php
@@ -465,7 +465,7 @@ class WebService
 					'name' => $cacheid2wptcode[$row['cache_id']]."-".($row['stage'] ? $row['stage'] : "wpt"),
 					'location' => round($row['latitude'], 6)."|".round($row['longitude'], 6),
 					'sym' => (($row['type'] == 3) ? "Flag, Red" : (($row['type'] == 4) ? "Circle with X" : 
-               (($row['type'] == 5) ? "Parking Area" : "Flag, Green"))),
+						(($row['type'] == 5) ? "Parking Area" : "Flag, Green"))),
 					'description' => ($row['stage'] ? _("Stage")." ".$row['stage'].": " : "").$row['desc'],
 				);
 			}
