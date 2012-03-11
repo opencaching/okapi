0dbde5d38c393df67f8699769d60a4009d96186e
rygielski <rygielski@gmail.com>
2012-03-11 11:44:56 +0000

    Fixed invalid output buffering usage.

--- okapi/views/signup.php
+++ okapi/views/signup.php
@@ -71,7 +71,7 @@ class View
 		$response->content_type = "text/html; charset=utf-8";
 		ob_start();
 		include 'signup.tpl.php';
-		$response->body = ob_get_flush();
+		$response->body = ob_get_clean();
 		return $response;
 	}
 }
