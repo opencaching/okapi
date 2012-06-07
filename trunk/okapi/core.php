9c0f32aefe3d32d455d6a84d1e429ca8a84272e7
rygielski@gmail.com <rygielski@gmail.com@9d75f559-08eb-57be-2bd7-6ad0879b0bc2>
2012-06-07 21:25:29 +0000

    OKAPI's error response for invalid (revoked) token was "invalid_signature" instead of "invalid_token". Fixed.

--- trunk/okapi/core.php
+++ trunk/okapi/core.php
@@ -511,11 +511,13 @@ class OkapiOAuthServer extends OAuthServer
 		$consumer = $this->get_consumer($request);
 		try {
 			$token = $this->get_token($request, $consumer, $token_type);
-		} catch (OAuthServerException $e) {
-			if ($token_required)
-				throw $e;
-			else
+		} catch (OAuthMissingParameterException $e) {
+			# Note, that exception will be different if token is supplied
+			# and is invalid. We catch only a completely MISSING token parameter.
+			if (($e->getParamName == 'oauth_token') && (!$token_required))
 				$token = null;
+			else
+				throw $e;
 		}
 		$this->check_signature($request, $consumer, $token);
 		return array($consumer, $token);
