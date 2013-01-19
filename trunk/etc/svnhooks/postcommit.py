06ace9058734a4963e06986b59d256bc31057bbd
rygielski <rygielski@9d75f559-08eb-57be-2bd7-6ad0879b0bc2>
2013-01-19 12:22:15 +0000

    New postcommit.py script.
    It was moved to a different server which required some changes.

--- trunk/etc/svnhooks/postcommit.py
+++ trunk/etc/svnhooks/postcommit.py
@@ -19,131 +19,160 @@
 #     all the files which might have been removed will REMAIN),
 #   - commits the changes to opencaching-pl project.
 
-print "Content-Type: text/plain; charset=utf-8"
-print
-print "Hello there!"
-print
-
+import contextlib
 import sys
-
-try:
-	from auth import okapi_auth_key, okapi_username, okapi_password, ocpl_username, ocpl_password
-except ImportError:
-	print "A file called auth.py is required to run this script. The contents are:"
-	print
-	print 'okapi_auth_key = "Post-Commit Authentication Key for opencaching-api project"'
-	print 'okapi_username = "Username with RW rights to opencaching-api project"'
-	print 'okapi_password = "User\'s commit-password"'
-	print 'ocpl_username = "Username with RW rights to opencaching-pl project"'
-	print 'ocpl_password = "User\'s commit-password"'
-	sys.exit(1)
-	
 import cgi
 import cgitb
-cgitb.enable()
 import os
 import hmac
 import json
+import datetime
+import subprocess
+from cStringIO import StringIO
+
 from googlecode_upload import upload_find_auth
 
-# Reading request body and signature.
 
-body = sys.stdin.read()
-# e.g. body = '{"repository_path":"http://opencaching-api.googlecode.com/svn/","project_name":"opencaching-api","revisions":[{"added":["/trunk/test.txt"],"author":"rygielski@gmail.com","url":"http://opencaching-api.googlecode.com/svn-history/r23/","timestamp":1313750735,"message":"Testing SVN hooks.","path_count":1,"removed":[],"modified":[],"revision":23}],"revision_count":1}'
-signature = os.environ['HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC'] if os.environ.has_key('HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC') else "none"
-# e.g. signature = '18daa1cd537cb6d5f7eda2935f81b0fb'
+@contextlib.contextmanager
+def capture():
+	oldout,olderr = sys.stdout, sys.stderr
+	try:
+		out=[StringIO(), StringIO()]
+		sys.stdout,sys.stderr = out
+		yield out
+	finally:
+		sys.stdout,sys.stderr = oldout, olderr
+		out[0] = out[0].getvalue()
+		out[1] = out[1].getvalue()
+		# After the script terminates, write the output to stdout.
+		print out[0]
+		# Also, save it to a log file.
+		now = datetime.datetime.now()
+		name = 'request-' + '%s%02d%02d-%02d%02d%02d-%06d' % (now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+		with open(name + '.out', 'w') as f:
+			f.write(out[0])
+		# Also, save the errors to an error log file.
+		if out[1]:
+			with open(name + '.err', 'w') as f:
+				f.write(out[1])
 
-print "Your request body is:"
-print
-print body
-print
-print "And your signature is: " + signature
-print
+with capture() as out:
 
-# Validating the signature.
-
-m = hmac.new(okapi_auth_key)
-m.update(body)
-digest = m.hexdigest()
-if digest == signature:
-	print "Signature is VALID."
-else:
-	print "Signature is INVALID. Aborting your request."
-	sys.exit(1)
-
-data = json.loads(body)
-filenames = []
-revision = None
-for rev_data in data['revisions']:
-	for key in ['added', 'modified', 'removed']:
-		for filename in rev_data[key]:
-			filenames.append(filename)
-	revision = rev_data['revision']
-print
-print "Files affected:\n" + "\n".join(filenames)
-print
-
-important_filenames = filter(lambda filename: filename.startswith("/trunk/"), filenames)
-important_filenames = filter(lambda filename: not filename.startswith("/trunk/etc/"), important_filenames)
-if len(important_filenames) == 0:
-	print "Deployment package was unaffected by this commit. Aborting."
-	sys.exit(0)
-
-import subprocess
-
-deployment_name = "okapi-r" + str(revision)
-try:
-	print "Exporting revision " + str(revision) + "..."
-	sys.stdout.flush()
-	subprocess.call(["svn", "export", "http://opencaching-api.googlecode.com/svn/trunk/",
-		deployment_name, "-r" + str(revision)], stdout=sys.stdout, stderr=sys.stdout)
-	sys.stdout.flush()
-	print "Removing files not intended for deployment..."
-	subprocess.call(["rm", "-rf", deployment_name + "/etc"])
-	print "Adding version information..."
-	fp = open(deployment_name + '/okapi/core.php', 'r')
-	core_contents = fp.read()
-	fp.close()
-	core_contents = core_contents.replace(
-		"public static $revision = null;",
-		"public static $revision = " + str(revision) + ";")
-	fp = open(deployment_name + '/okapi/core.php', 'w')
-	fp.write(core_contents)
-	fp.close()
-	print "Creating archive..."
-	sys.stdout.flush()
-	subprocess.call(["tar", "-czf", deployment_name + ".tar.gz", deployment_name])
-	subprocess.call(["chmod", "666", deployment_name + ".tar.gz"])
-	subprocess.call(["rm", "-rf", deployment_name])
-	print "Uploading to the Downloads page..."
-	sys.stdout.flush()
-	upload_find_auth(deployment_name + ".tar.gz", "opencaching-api",
-		"OKAPI revision " + str(revision) + " (automatic deployment)",
-		user_name=okapi_username, password=okapi_password)
-	print "Checking out opencaching-pl/trunk/okapi..."
-	sys.stdout.flush()
-	subprocess.call(["svn", "co", "https://opencaching-pl.googlecode.com/svn/trunk/okapi",
-		deployment_name + "/okapi"], stdout=sys.stdout, stderr=sys.stdout)
-	sys.stdout.flush()
-	print "Replacing opencaching.pl's okapi contents with the latest version..."
-	## Currently I have only svn 1.6 on the server where this script is being run.
-	## Therefore, I cannot simply remove all the contents (.svn directories need to
-	## be there). This can be fixed once svn on my server gets upgraded.
-	# subprocess.call(["rm", "-rf", deployment_name + "/*"], shell=True)
-	subprocess.call(["tar", "--overwrite", "-xf", deployment_name + ".tar.gz"],
-		stdout=sys.stdout, stderr=sys.stdout)
-	subprocess.call(["svn", "add", "--force", "."], cwd = deployment_name + "/okapi",
-		stdout=sys.stdout, stderr=sys.stdout)
-	subprocess.call(["svn", "commit", deployment_name + "/okapi", "--non-interactive", "--username",
-		ocpl_username, "--password", ocpl_password, "--no-auth-cache", "-m",
-		"Automatic OKAPI Project update (r" + str(revision) + ")"],
-		stdout=sys.stdout, stderr=sys.stdout)
-	print "Cleanup..."
-	subprocess.call(["rm", "-rf", deployment_name])
-except OSError, e:
-	print "Error :("
-	print str(e)
-	sys.exit(1)
-
-print "Deployment complete."
-
+	print "Content-Type: text/plain; charset=utf-8"
+	print
+	print "Hello there!"
+	print
+	
+	try:
+		from auth import okapi_auth_key, okapi_username, okapi_password, ocpl_username, ocpl_password
+	except ImportError:
+		print "A file called auth.py is required to run this script. The contents are:"
+		print
+		print 'okapi_auth_key = "Post-Commit Authentication Key for opencaching-api project"'
+		print 'okapi_username = "Username with RW rights to opencaching-api project"'
+		print 'okapi_password = "User\'s commit-password"'
+		print 'ocpl_username = "Username with RW rights to opencaching-pl project"'
+		print 'ocpl_password = "User\'s commit-password"'
+		sys.exit(1)
+	
+	cgitb.enable()
+	
+	# Reading request body and signature.
+	
+	body = sys.stdin.read()
+	# e.g. body = '{"repository_path":"http://opencaching-api.googlecode.com/svn/","project_name":"opencaching-api","revisions":[{"added":["/trunk/test.txt"],"author":"rygielski@gmail.com","url":"http://opencaching-api.googlecode.com/svn-history/r23/","timestamp":1313750735,"message":"Testing SVN hooks.","path_count":1,"removed":[],"modified":[],"revision":23}],"revision_count":1}'
+	signature = os.environ['HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC'] if os.environ.has_key('HTTP_GOOGLE_CODE_PROJECT_HOSTING_HOOK_HMAC') else "none"
+	# e.g. signature = '18daa1cd537cb6d5f7eda2935f81b0fb'
+	
+	print "Your request body is:"
+	print
+	print body
+	print
+	print "And your signature is: " + signature
+	print
+	
+	# Validating the signature.
+	
+	m = hmac.new(okapi_auth_key)
+	m.update(body)
+	digest = m.hexdigest()
+	if digest == signature:
+		print "Signature is VALID."
+	else:
+		print "Signature is INVALID. Aborting your request."
+		sys.exit(1)
+	
+	data = json.loads(body)
+	filenames = []
+	revision = None
+	for rev_data in data['revisions']:
+		for key in ['added', 'modified', 'removed']:
+			for filename in rev_data[key]:
+				filenames.append(filename)
+		revision = rev_data['revision']
+	print
+	print "Files affected:\n" + "\n".join(filenames)
+	print
+	
+	important_filenames = filter(lambda filename: filename.startswith("/trunk/"), filenames)
+	important_filenames = filter(lambda filename: not filename.startswith("/trunk/etc/"), important_filenames)
+	if len(important_filenames) == 0:
+		print "Deployment package was unaffected by this commit. Aborting."
+		sys.exit(0)
+	
+	deployment_name = "okapi-r" + str(revision)
+	try:
+		print "Exporting revision " + str(revision) + "..."
+		sys.stdout.flush()
+		subprocess.call(["svn", "export", "http://opencaching-api.googlecode.com/svn/trunk/",
+			deployment_name, "-r" + str(revision)], stdout=sys.stdout, stderr=sys.stdout)
+		sys.stdout.flush()
+		print "Removing files not intended for deployment..."
+		subprocess.call(["rm", "-rf", deployment_name + "/etc"])
+		print "Adding version information..."
+		fp = open(deployment_name + '/okapi/core.php', 'r')
+		core_contents = fp.read()
+		fp.close()
+		core_contents = core_contents.replace(
+			"public static $revision = null;",
+			"public static $revision = " + str(revision) + ";")
+		fp = open(deployment_name + '/okapi/core.php', 'w')
+		fp.write(core_contents)
+		fp.close()
+		print "Creating archive..."
+		sys.stdout.flush()
+		subprocess.call(["tar", "-czf", deployment_name + ".tar.gz", deployment_name])
+		subprocess.call(["chmod", "666", deployment_name + ".tar.gz"])
+		subprocess.call(["setfacl", "-m", "u:rygielski:r", deployment_name + ".tar.gz"])
+		subprocess.call(["rm", "-rf", deployment_name])
+		print "Uploading to the Downloads page..."
+		sys.stdout.flush()
+		upload_find_auth(deployment_name + ".tar.gz", "opencaching-api",
+			"OKAPI revision " + str(revision) + " (automatic deployment)",
+			user_name=okapi_username, password=okapi_password)
+		print "Checking out opencaching-pl/trunk/okapi..."
+		sys.stdout.flush()
+		subprocess.call(["svn", "co", "https://opencaching-pl.googlecode.com/svn/trunk/okapi",
+			deployment_name + "/okapi"], stdout=sys.stdout, stderr=sys.stdout)
+		sys.stdout.flush()
+		print "Replacing opencaching.pl's okapi contents with the latest version..."
+		## Currently I have only svn 1.6 on the server where this script is being run.
+		## Therefore, I cannot simply remove all the contents (.svn directories need to
+		## be there). This can be fixed once svn on my server gets upgraded.
+		# subprocess.call(["rm", "-rf", deployment_name + "/*"], shell=True)
+		subprocess.call(["tar", "--overwrite", "-xf", deployment_name + ".tar.gz"],
+			stdout=sys.stdout, stderr=sys.stdout)
+		subprocess.call(["svn", "add", "--force", "."], cwd = deployment_name + "/okapi",
+			stdout=sys.stdout, stderr=sys.stdout)
+		subprocess.call(["svn", "commit", deployment_name + "/okapi", "--non-interactive", "--username",
+			ocpl_username, "--password", ocpl_password, "--no-auth-cache", "-m",
+			"Automatic OKAPI Project update (r" + str(revision) + ")"],
+			stdout=sys.stdout, stderr=sys.stdout)
+		print "Cleanup..."
+		subprocess.call(["rm", "-rf", deployment_name])
+	except OSError, e:
+		print "Error :("
+		print str(e)
+		sys.exit(1)
+	
+	print "Deployment complete."
