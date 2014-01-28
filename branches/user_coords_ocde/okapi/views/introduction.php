f05940c1585030bb14a92b6072d8a2cd4643d8e8
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@9d75f559-08eb-57be-2bd7-6ad0879b0bc2>
2014-01-28 20:29:17 +0000

    Branch for Issue #305

---
+++ branches/user_coords_ocde/okapi/views/introduction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace okapi\views\introduction;
+
+use Exception;
+use okapi\Okapi;
+use okapi\Settings;
+use okapi\OkapiRequest;
+use okapi\OkapiHttpResponse;
+use okapi\ParamMissing;
+use okapi\InvalidParam;
+use okapi\OkapiServiceRunner;
+use okapi\OkapiInternalRequest;
+use okapi\views\menu\OkapiMenu;
+use okapi\OkapiInternalConsumer;
+
+class View
+{
+    public static function call()
+    {
+        require_once($GLOBALS['rootpath'].'okapi/service_runner.php');
+        require_once($GLOBALS['rootpath'].'okapi/views/menu.inc.php');
+
+        $vars = array(
+            'menu' => OkapiMenu::get_menu_html("introduction.html"),
+            'okapi_base_url' => Settings::get('SITE_URL')."okapi/",
+            'site_url' => Settings::get('SITE_URL'),
+            'method_index' => OkapiServiceRunner::call('services/apiref/method_index',
+                new OkapiInternalRequest(new OkapiInternalConsumer(), null, array())),
+            'installations' => OkapiMenu::get_installations(),
+            'okapi_rev' => Okapi::$revision,
+        );
+
+        $response = new OkapiHttpResponse();
+        $response->content_type = "text/html; charset=utf-8";
+        ob_start();
+        include 'introduction.tpl.php';
+        $response->body = ob_get_clean();
+        return $response;
+    }
+}
