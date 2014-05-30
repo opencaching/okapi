5410c19170e2e62a3980413d07cb57b568773d78
boguslaw.szczepanowski@gmail.com <boguslaw.szczepanowski@gmail.com@9d75f559-08eb-57be-2bd7-6ad0879b0bc2>
2014-05-30 21:24:34 +0000

    Detailed cache access logging

---
+++ branches/cache_access_logging/okapi/services/apiref/method_index.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace okapi\services\apiref\method_index;
+
+use Exception;
+use okapi\Okapi;
+use okapi\OkapiRequest;
+use okapi\ParamMissing;
+use okapi\InvalidParam;
+use okapi\OkapiServiceRunner;
+use okapi\Cache;
+use okapi\OkapiInternalConsumer;
+use okapi\OkapiInternalRequest;
+
+
+class WebService
+{
+    public static function options()
+    {
+        return array(
+            'min_auth_level' => 0
+        );
+    }
+
+    public static function call(OkapiRequest $request)
+    {
+        $methodnames = OkapiServiceRunner::$all_names;
+        sort($methodnames);
+        $cache_key = "api_ref/method_index#".md5(implode("#", $methodnames));
+        $results = Cache::get($cache_key);
+        if ($results == null)
+        {
+            $results = array();
+            foreach ($methodnames as $methodname)
+            {
+                $info = OkapiServiceRunner::call('services/apiref/method', new OkapiInternalRequest(
+                    new OkapiInternalConsumer(), null, array('name' => $methodname)));
+                $results[] = array(
+                    'name' => $info['name'],
+                    'brief_description' => $info['brief_description'],
+                );
+            }
+            Cache::set($cache_key, $results, 3600);
+        }
+        return Okapi::formatted_response($request, $results);
+    }
+}
