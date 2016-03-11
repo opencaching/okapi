d0178db5b6d47aa23c011e40a225a4fcb5e07064
following <following@online.de>
2016-03-11 22:52:31 +0100

    re-added full parameter validation for logtypename2id()

--- okapi/core.php
+++ okapi/core.php
@@ -1835,7 +1835,7 @@ class Okapi
         if ($name == 'Comment') return 3;
         if ($name == 'Attended') return 7;
         if ($name == 'Will attend') return 8;
-        if ($name == 'Needs maintenance') return 5;
+        if (($name == 'Needs maintenance') && (Settings::get('OC_BRANCH') == 'oc.pl')) return 5;
         throw new Exception("logtype2id called with invalid log type argument: $name");
     }
 
