037ba672cc13418e9e5e5e98dcad7043e2a89ea4
following <following@online.de>
2016-04-04 01:39:51 +0200

    added definitions of users/send_email and caches/report_problem

---
+++ okapi/services/users/send_email.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace okapi\services\users\send_email;
+
+use okapi\BadRequest;
+
+use okapi\OkapiInternalRequest;
+
+use okapi\OkapiServiceRunner;
+
+use okapi\Okapi;
+use okapi\OkapiRequest;
+use okapi\ParamMissing;
+use okapi\InvalidParam;
+use okapi\services\caches\search\SearchAssistant;
+
+class WebService
+{
+    public static function options()
+    {
+        return array(
+            'min_auth_level' => 3
+        );
+    }
+
+    public static function call(OkapiRequest $request)
+    {
+    }
+}
