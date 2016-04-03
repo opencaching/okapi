e3b34ddf4a4e8c17dc3f6f2b533727dc3f7772ad
following <following@online.de>
2016-04-03 16:45:44 +0200

    inform developers about the submit-charset

--- okapi/services/apisrv/installation.php
+++ okapi/services/apisrv/installation.php
@@ -18,6 +18,20 @@ class WebService
 
     public static function call(OkapiRequest $request)
     {
+        switch (Settings::get('DB_CHARSET'))
+        {
+            case 'utf8':
+                $submit_charset = 'utf8mb2';
+                break;
+
+            case 'utf8mb4':
+                $submit_charset = 'utf8mb4';
+                break;
+
+            default:
+                throw new Exception('Unknown DB_CHARSET: ' . Settings::get('DB_CHARSET'));
+        }
+
         $result = array();
         $result['site_url'] = Settings::get('SITE_URL');
         $result['okapi_base_url'] = $result['site_url']."okapi/";
@@ -27,6 +41,7 @@ class WebService
         $result['okapi_git_revision'] = Okapi::$git_revision;
         $result['registration_url'] = Settings::get('REGISTRATION_URL');
         $result['mobile_registration_url'] = Settings::get('MOBILE_REGISTRATION_URL');
+        $result['submit_charset'] = $submit_charset;
         $result['image_max_upload_size'] = Settings::get('IMAGE_MAX_UPLOAD_SIZE');
         $result['image_rcmd_max_pixels'] = Settings::get('IMAGE_MAX_PIXEL_COUNT');
         return Okapi::formatted_response($request, $result);
