0849b401d86dcd5dbac2db621e2cf83d24c5dc45
Thomas Eiling <teiling88@gmail.com>
2017-08-24 21:58:01 +0200

    reformat okapi\views\signup namespace

--- okapi/views/signup/View.php
+++ okapi/views/signup/View.php
@@ -3,7 +3,7 @@
 namespace okapi\views\signup;
 
 use okapi\Okapi;
-use okapi\OkapiHttpResponse;
+use okapi\Response\OkapiHttpResponse;
 use okapi\Settings;
 use okapi\views\menu\OkapiMenu;
 
@@ -47,8 +47,6 @@ class View
             ));
             return $response;
         }
-
-        require_once __DIR__ . '/menu.inc.php';
 
         $vars = array(
             'menu' => OkapiMenu::get_menu_html("signup.html"),
