4244b24e43112476d1d70a2dd0560aa630a5a928
following <following@online.de>
2018-11-16 13:55:58 +0100

    init OKAPI exceptions before calling Settings:get()

--- okapi/Settings.php
+++ okapi/Settings.php
@@ -283,6 +283,8 @@ final class Settings
     {
         if (!in_array($dict['OC_BRANCH'], array('oc.pl', 'oc.de')))
             throw new Exception("Currently, OC_BRANCH has to be either 'oc.pl' or 'oc.de'. Hint: Whom did you get your code from?");
+        if ($dict['OC_BRANCH'] == 'oc.pl' && $dict['EXTERNAL_AUTOLOADER'] === null)
+            throw new Exception("OCPL autoloader is not configured");
         $boolean_keys = array('DEBUG', 'DEBUG_PREVENT_EMAILS', 'DEBUG_PREVENT_SEMAPHORES', 'USE_SQL_SUBQUERIES');
         foreach ($boolean_keys as $key)
             if (!in_array($dict[$key], array(true, false)))
